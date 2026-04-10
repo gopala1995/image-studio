348a35ceab3be3b7f588baa5a43a2d31ab7506a9
Gopala <ggopala1904@gmail.com>
2026-04-10 12:56:58 +0530

    feat: integrate Sentry for error tracking and monitoring
    - Added Sentry dependency to package.json
    - Created configuration files for Sentry on edge and server
    - Included various image assets for project use

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e9be8eaf753d96a41060d6b43766fb4b@o4511194465370112.ingest.us.sentry.io/4511194471596032",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
